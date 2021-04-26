075bb886249378fdd60e15c6c70fa6bd03f0532e
Evance23 <evancebarracks01@gmail.com>
2021-04-27 01:27:14 +0300

    Add get and post funtionalities for the log in form

--- app/main/views.py
+++ app/main/views.py
@@ -4,31 +4,37 @@ from app import app
 from app.form import LoginForm
 
 # Views
+
+
 @app.route('/')
 def index():
     title = 'Home- Making the minute count'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title=title)
 
 # @app.route('/navbar.html')
 # def navbar():
 #     return render_template('navbar')
 
-@app.route('/login')
+
+@app.route('/login' methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('User Log in {}, remember_me{} .format(form.username.data, form.remmeber_me.data)
+        return redirect('/index')
     return render_template('login.html', title='Log In', form=form)
 
-@app.route('/profile.html')
+@ app.route('/profile.html')
 def profie():
     return ender_template('profile.html')
 
-@app.route('/signup.html')
+@ app.route('/signup.html')
 def signup():
     return render_template('signup.html')
 
-@app.route('/comment.html')
+@ app.route('/comment.html')
 def comment():
     return render_template('comment.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
