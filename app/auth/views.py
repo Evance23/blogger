ad19d7a5926b5ee256ecb9c4e2f502e696d774d0
Evance23 <evancebarracks01@gmail.com>
2021-04-28 05:37:34 +0300

    Add signup and log out funtionalities

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from app import app
 from . import auth
 
 
-from flask_login import login_user, logout_user ,login_required
+from flask_login import login_user, logout_user, login_required
 from ..models import User
 
 from .forms import RegistrationForm
@@ -44,20 +44,45 @@ def login():
         return redirect('/')
     return render_template('login.html')
 
-
-@ app.route('/profile.html')
-def profie():
-    return ender_template('profile.html')
-
-
-@ app.route('/signup.html')
+    @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        form = request.form
+        username = form.get("username")
+        email = form.get("email")
+        password = form.get("password")
+        confirm_password = form.get("confirm_password")
+        if username is None or password is None or email is None or confirm_password is None:
+            error = 'username, email, password are required'
+            return render_template('signup.html', error=error)
+        if ' ' in username:
+            error = 'Username should not contain spaces'
+            return render_template('signup.html', error=error)
+        if password != confirm_password:
+            error = "Passwords do not match"
+            return render_template('signup.html', error=error)
+        else:
+            user = User.query.filter_by(username=username).first()
+            if user is not None:
+                error = 'A user with that name already exists'
+                return render_template('signup.html', error=error)
+            user = User.query.filter_by(email=email).first()
+            if user is not None:
+                error = 'A user with that email already exists'
+                return render_template('signup.html', error=error)
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # email_message("Welcome to 1m Pitch", "email/howdy", user.email, user=user)
+            return redirect(url_for('auth.login'))
+
     return render_template('signup.html')
 
 
-@ app.route('/comment.html')
-def comment():
-    return render_template('comment.html')
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 if __name__ == '__main__':
