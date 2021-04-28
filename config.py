6e74fdf1d87477788bd2d2d2313d0e6d0cd95c00
Evance23 <evancebarracks01@gmail.com>
2021-04-28 07:06:10 +0300

    Add elements to the config class

--- config.py
+++ config.py
@@ -7,7 +7,19 @@ class Config:
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-pass
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+# simple mde configurations
+SIMPLEMDE_JS_IIFE = True
+SIMPLEMDE_USE_CDN = True
+
+
+@staticmethod
+def init_app(app):
+    pass
 
 
 class ProdConfig(Config):
@@ -29,3 +41,10 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
