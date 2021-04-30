e647610cccceb2219a410c446490fef82afd1084
Evance23 <evancebarracks01@gmail.com>
2021-04-30 15:46:07 +0300

    Add modifications in models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 
 from . import db ,login_manager
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
+# from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage 
 from flask_login import UserMixin , current_user
 
 class User(UserMixin, db.Model):
