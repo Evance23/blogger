864119d4f9e5cfac537af6456b6a2d216dc6fa41
Evance23 <evancebarracks01@gmail.com>
2021-12-30 16:53:38 +0300

    Add new fles and pages

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,11 +25,12 @@ def index():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
+    login_form = LoginForm(csrf_enable=False)
+    form = LoginForm(csrf_enable=False)
     if login_form.validate_on_submit():
         # password = login_form.get('password')
         user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.check_password(login_form.password.data):
+        if user != None and user.verify_password(form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
